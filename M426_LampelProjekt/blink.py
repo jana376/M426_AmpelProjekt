983e7b67657637db66ca44820fc7486e20a9f6d6
zainab kalejaiye <zainab_kalejaiye@sluz.ch>
2026-06-05 15:09:09 +0200

    ProjektVorlage

---
+++ M426_LampelProjekt/blink.py
@@ -0,0 +1,23 @@
+from machine import Pin
+from utime import sleep
+
+onBoard = Pin("LED", Pin.OUT)
+led1 = Pin(8, Pin.OUT)
+led2 = Pin(18, Pin.OUT)
+
+while True:
+    try:
+        onBoard.toggle()
+        led1.toggle()
+        led2.toggle()
+        sleep(1) # sleep 1sec
+    except KeyboardInterrupt:
+        break
+        sleep(1) # sleep 1sec
+    except KeyboardInterrupt:
+        break
+
+onBoard.off()
+led1.off()
+led2.off()
+print("Finished.")
